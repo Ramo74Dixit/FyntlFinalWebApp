b0dee02f9612a375933af841ebf9e8dce9c1946d
Aabish01 <aabishk23@gmail.com>
2024-09-05 00:42:09 +0530

    worked for responsiveness

--- frontend/src/page/CopilotIntro.js
+++ frontend/src/page/CopilotIntro.js
@@ -10,11 +10,11 @@ const CopilotIntro = () => {
       <div className="copilot-navbar-style">
         <Navbar />
       </div>
-      <div className="flex flex-col justify-center items-center font-roboto p-10 leading-relaxed m-8">
-        <div className="w-2/3 text-3xl font-bold mb-4 text-center">
+      <div className="flex flex-col justify-center items-center font-roboto p-10 leading-relaxed m-0 sm:m-8 ">
+        <div className="w-full sm:w-2/3 text-3xl font-bold mb-4 text-center">
           Tired of spending hours on repetitive tasks?
         </div>
-        <div className="w-3/5 text-xl text-center opacity-50">
+        <div className="w-full sm:w-3/5 text-xl text-center opacity-50">
           Let Accounting Co-pilot be your secret weapon. This innovative AI platform
           automates tedious processes, streamlines workflows, and frees you up to focus 
           on what matters most - building strong client relationships and delivering exceptional service.
